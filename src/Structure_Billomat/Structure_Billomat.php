a7f50b5b2c6d4097572a6195c19964ff8609dbba
Felix Arntz <felix-arntz@leaves-and-love.net>
2017-03-29 11:52:05 +0200

    Set API title.

--- src/Structure_Billomat/Structure_Billomat.php
+++ src/Structure_Billomat/Structure_Billomat.php
@@ -28,7 +28,7 @@ if ( ! class_exists( 'APIAPI\Structure_Billomat\Structure_Billomat' ) ) {
 		 * @access protected
 		 */
 		protected function setup() {
-			$this->title         = 'Billomat';
+			$this->title         = 'Billomat API';
 			$this->base_uri      = 'https://{billomatID}.billomat.net/api/';
 			$this->authenticator = 'x-account';
 
