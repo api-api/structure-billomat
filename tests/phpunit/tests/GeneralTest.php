6f5ee0cb9cb2c06a974fdcde4ee99d6db7e3471c
Sven Wagener <sven@awesome.ug>
2017-12-11 17:23:59 +0100

    Trying tests

--- tests/phpunit/tests/GeneralTest.php
+++ tests/phpunit/tests/GeneralTest.php
@@ -4,16 +4,11 @@ require_once dirname( dirname( __FILE__ ) ) . '/includes/bootstrap.php';
 
 class GeneralTests extends Billomat_TestCase {
 	public function testThat() {
-		try {
-			$request = $this->apiapi->get_request_object( 'billomat', '/clients' );
-			$request->set_param( 'first_name', 'Felix' );
-			$response = $this->apiapi->send_request( $request );
+		$request = $this->apiapi->get_request_object( 'billomat', '/clients' );
+		$request->set_param( 'first_name', 'Felix' );
+		$response = $this->apiapi->send_request( $request );
 
-			$params = $response->get_params();
-			print_r( $response );
-
-		}catch ( Exception $e ) {
-			echo 'Exception: ',  $e->getMessage(), "\n";
-		}
+		$params = $response->get_params();
+		print_r( $response );
 	}
 }
