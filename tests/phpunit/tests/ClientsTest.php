ede05f9f31edbc206a098a7bbd217d5ffa7f2731
Sven Wagener <sven@awesome.ug>
2017-12-12 19:18:38 +0100

    Added new Test for adding a client

---
+++ tests/phpunit/tests/ClientsTest.php
@@ -0,0 +1,17 @@
+<?php
+
+require_once dirname( dirname( __FILE__ ) ) . '/includes/bootstrap.php';
+
+class ClientsTestTests extends Billomat_TestCase {
+	public function testCreateClient() {
+		$request = $this->apiapi->get_request_object( 'billomat', '/clients', 'POST' );
+
+		$request->set_param( 'name', 'Musterfirma' );
+		$request->set_param( 'first_name', 'Peter' );
+		$request->set_param( 'last_name', 'Mustermann' );
+		$response = $this->apiapi->send_request( $request );
+
+		$params = $response->get_params();
+		print_r( $response );
+	}
+}
