174365bebec2d26df25ff064980445b5249976d8
Sven Wagener <sven@awesome.ug>
2018-07-12 10:59:33 +0200

    Added invoice endpoints (not finished)

--- src/Structure_Billomat/Structure_Billomat.php
+++ src/Structure_Billomat/Structure_Billomat.php
@@ -285,12 +285,293 @@ if ( ! class_exists( 'APIAPI\Structure_Billomat\Structure_Billomat' ) ) {
 			$this->routes['/clients/(?P<id>[\\d]+)'] = array(
 				'methods' => array(
 					Method::GET => array(
-						'description'          => 'Removes the uploaded profile banner for the authenticating user. Returns HTTP 200 upon success.',
+						'description'          => 'Returns data of a client.',
 						'needs_authentication' => true,
 						'params'               => array(),
 					),
 				),
 			);
+
+			$this->routes['/invoices/(?P<id>[\\d]+)'] = array(
+				'methods' => array(
+					Method::GET => array(
+						'description'          => 'Returns data of an invoice.',
+						'needs_authentication' => true,
+						'params'               => array(),
+					),
+				),
+			);
+
+			$this->routes['/invoices'] = array(
+				'methods' => array(
+					Method::POST => array(
+						'description'          => 'Creates a new bill.',
+						'needs_authentication' => true,
+						'params'               => array(
+							'client_id'        => array(
+								'description' => 'Customer ID',
+								'type'        => 'integer',
+							),
+							'contact_id'        => array(
+								'description' => 'Contact ID',
+								'type'        => 'integer',
+								'required'    => false,
+							),
+							'address'        => array(
+								'description' => 'Invoice address',
+								'type'        => 'string',
+								'required'    => false,
+							),
+							'number_pre'        => array(
+								'description' => 'Number Prefix',
+								'type'        => 'string',
+								'required'    => false,
+							),
+							'number'        => array(
+								'description' => 'Number',
+								'type'        => 'integer',
+								'required'    => false,
+							),
+							'number_length'        => array(
+								'description' => 'Number length',
+								'type'        => 'integer',
+								'required'    => false,
+							),
+							'date'        => array(
+								'description' => 'Invoice Date',
+								'type'        => 'string',
+								'required'    => false,
+							),
+							'supply_date'        => array(
+								'description' => 'Supply Date',
+								'type'        => 'string',
+								'required'    => false,
+							),
+							'supply_date_type'        => array(
+								'description' => 'Supply Date Type (Available values: SUPPLY_DATE, DELIVERY_DATE, SUPPLY_TEXT, DELIVERY_TEXT)',
+								'type'        => 'string',
+								'required'    => false,
+							),
+							'due_days'        => array(
+								'description' => 'Due Days',
+								'type'        => 'integer',
+								'required'    => false,
+							),
+							'discount_rate'        => array(
+								'description' => 'Discount Rate (in percent)',
+								'type'        => 'integer',
+								'required'    => false,
+							),
+							'discount_days'        => array(
+								'description' => 'Discount Days',
+								'type'        => 'integer',
+								'required'    => false,
+							),
+							'discount_date'        => array(
+								'description' => 'Discount Date',
+								'type'        => 'string',
+								'required'    => false,
+							),
+							'title'        => array(
+								'description' => 'Document Title',
+								'type'        => 'string',
+								'required'    => false,
+							),
+							'label'        => array(
+								'description' => 'Document Label',
+								'type'        => 'string',
+								'required'    => false,
+							),
+							'intro'        => array(
+								'description' => 'Intro Text',
+								'type'        => 'string',
+								'required'    => false,
+							),
+							'note'        => array(
+								'description' => 'Note',
+								'type'        => 'string',
+								'required'    => false,
+							),
+							'reduction'        => array(
+								'description' => 'Price Reduction (in percent)',
+								'type'        => 'string',
+								'required'    => false,
+							),
+							'currency_code'        => array(
+								'description' => 'Currency Code (ISO)',
+								'type'        => 'string',
+								'required'    => false,
+							),
+							'net_gross'        => array(
+								'description' => 'Price Base (Available values: NET, GROSS)',
+								'type'        => 'string',
+								'required'    => false,
+							),
+							'quote'        => array(
+								'description' => 'Currency Rate',
+								'type'        => 'string',
+								'required'    => false,
+							),
+							'payment_types'        => array(
+								'description' => 'Payment Types (Comma separated)',
+								'type'        => 'string',
+								'required'    => false,
+							),
+							'invoice_id'        => array(
+								'description' => 'Invoice ID',
+								'type'        => 'integer',
+								'required'    => false,
+							),
+							'offer_id'        => array(
+								'description' => 'Offer ID',
+								'type'        => 'integer',
+								'required'    => false,
+							),
+							'confirmation_id'        => array(
+								'description' => 'Confirmation ID',
+								'type'        => 'integer',
+								'required'    => false,
+							),
+							'recurring_id'        => array(
+								'description' => 'Recurring ID',
+								'type'        => 'integer',
+								'required'    => false,
+							),
+							'free_text_id'        => array(
+								'description' => 'Free Text ID',
+								'type'        => 'integer',
+								'required'    => false,
+							),
+							'template_id'        => array(
+								'description' => 'Template ID',
+								'type'        => 'integer',
+								'required'    => false,
+							),
+							// Invoice Items?
+						),
+					),
+				),
+			);
+
+			$this->routes['/invoices/(?P<id>[\\d]+)'] = array(
+				'methods' => array(
+					Method::PUT => array(
+						'description'          => 'Edits an invoice.',
+						'needs_authentication' => true,
+						'params'               => array(
+							'client_id'        => array(
+								'description' => 'Customer ID',
+								'type'        => 'integer',
+							),
+						),
+					),
+					Method::DELETE => array(
+						'description'          => 'Edits an invoice.',
+						'needs_authentication' => true,
+						'params'               => array(
+							'client_id'        => array(
+								'description' => 'Customer ID',
+								'type'        => 'integer',
+							),
+						),
+					),
+				)
+			);
+
+			$this->routes['/invoices/(?P<id>[\\d]+)/complete'] = array(
+				'methods' => array(
+					Method::PUT => array(
+						'description'          => 'Completes an invoice.',
+						'needs_authentication' => true,
+					),
+				),
+			);
+
+			$this->routes['/invoices/(?P<id>[\\d]+)/pdf'] = array(
+				'methods' => array(
+					Method::GET => array(
+						'description'          => 'Returns an invoice PDF.',
+						'needs_authentication' => true,
+					),
+				),
+			);
+
+			$this->routes['/invoices/(?P<id>[\\d]+)/email'] = array(
+				'methods' => array(
+					Method::POST => array(
+						'description'          => 'Sends an invoice via email.',
+						'needs_authentication' => true,
+						'params'               => array(
+							'email_template_id'   => array(
+								'description'     => 'Email Template ID',
+								'type'            => 'integer',
+								'required'    => false,
+							),
+							'from'   => array(
+								'description'     => 'Email From Address',
+								'type'            => 'string',
+								'required'    => false,
+							),
+							'recipients'   => array(
+								'description'     => 'Email Recipients',
+								'type'            => 'integer',
+								'required'    => true,
+								// Sub parameters
+							),
+							'subject'   => array(
+								'description'     => 'Email Subject',
+								'type'            => 'string',
+							),
+							'body'   => array(
+								'description'     => 'Email Body',
+								'type'            => 'integer',
+							),
+							'filename'   => array(
+								'description'     => 'Invoice Filename',
+								'type'            => 'integer',
+							),
+							'attachments'   => array(
+								'description'     => 'Email Template ID',
+								'type'            => 'integer',
+								// Sub parameters
+							),
+						),
+					),
+				),
+			);
+
+			$this->routes['/invoices/(?P<id>[\\d]+)/mail'] = array(
+				'methods' => array(
+					Method::POST => array(
+						'description'          => 'Sends an invoice via email.',
+						'needs_authentication' => true,
+						'params'               => array(
+							'color'   => array(
+								'description'     => 'Color Print (Available values: true or false)',
+								'type'            => 'boolean',
+								'required'    => false,
+							),
+							'duplex'   => array(
+								'description'     => 'Duplex Print (Available values: true or false)',
+								'type'            => 'boolean',
+								'required'    => false,
+							),
+							'paper_weight'   => array(
+								'description'     => 'Paper weight (Available values: 80, 90)',
+								'type'            => 'integer',
+								'required'    => false,
+							),
+							'attachments'   => array(
+								'description'     => 'PDF files which have to be printed too',
+								'type'            => 'boolean',
+								'required'    => false,
+								// Subparams
+							),
+						),
+					),
+				),
+			);
+
 		}
 	}
 }
